3a24323b353248f7478a7b50267ac59162b1023e
cdss1234 <ahmedshaikh78699@gmail.com>
2021-10-04 18:09:01 +0530

    Testing

--- Home.aspx.cs
+++ Home.aspx.cs
@@ -11,7 +11,7 @@ namespace GitDemo
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string strNav = "New Project";
+            string strNav = "New Project;Testing";
         }
     }
 }
